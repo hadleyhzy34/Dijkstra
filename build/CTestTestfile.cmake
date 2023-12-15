e7c075f61774dfb2b3a7ec02f5b5dfce81868aae
hadleyhzy34 <hadley_hzy@hotmail.com>
2023-12-16 01:29:49 +0800

    update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hadley/Developments/Dijkstra
-# Build directory: /home/hadley/Developments/Dijkstra/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
